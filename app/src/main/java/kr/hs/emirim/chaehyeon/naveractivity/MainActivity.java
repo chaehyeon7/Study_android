bb31cdf7277a97db28f39c2972a9ac42bbeafa84
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-03 21:38:03 +0900

    Edit Add BookMode, book_item.xml Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/naveractivity/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/naveractivity/MainActivity.java
@@ -3,12 +3,17 @@ package kr.hs.emirim.chaehyeon.naveractivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    static EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.search);
     }
 }
