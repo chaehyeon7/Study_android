151dea63db93018b6ad642fd73e80ab4de37204b
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-09 17:36:59 +0900

    Edit ViewModelAdapter.java (이미지 로드) commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/naveractivity/ViewModelAdapter.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/naveractivity/ViewModelAdapter.java
@@ -2,16 +2,20 @@ package kr.hs.emirim.chaehyeon.naveractivity;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.io.BufferedInputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ViewModelAdapter extends ArrayAdapter<BookModel> {
@@ -48,20 +52,48 @@ public class ViewModelAdapter extends ArrayAdapter<BookModel> {
         author.setText("author :" + vo.getB_author());
         price.setText("price :" + vo.getB_price());
 
+        //이미지 로드
+//        new ImgAsync( img,vo).execute();
+
         return convertView;
+
     }//getVIew()
 
-    //통신을 통해 이미지를 로드할 Async클래ㅡㅅ
+    //통신을 통해 이미지를 로드할 Async클래스
     class ImgAsync extends AsyncTask<String, String, Bitmap>{
+
+        Bitmap bm;
+        ImageView mlmg;
+        BookModel model;
+
+        public ImgAsync(ImageView mlmg, BookModel model) {
+            this.mlmg = mlmg;
+            this.model = model;
+        }
 
         @Override
         protected Bitmap doInBackground(String... strings) {
-            return null;
+
+            try {
+
+                URL img_url = new URL( model.getB_img() );
+                //얻어온 경로를 통해 로드작업을 수행(스트림 사용)
+                BufferedInputStream bis = new BufferedInputStream( img_url.openStream() );
+
+                //(얻어온 스트림으로부터) Bitmap객체 생성
+                bm = BitmapFactory.decodeStream( bis );
+
+                bis.close();
+
+            }catch (Exception e){
+
+            }
+            return bm;
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
-
+            mlmg.setImageBitmap( bitmap );
         }
     }
 }
